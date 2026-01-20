711d88e6ae1314fe3e0e41b16746eb8fcfba0a07
Ujvari.Csongor <Ujvari.Csongor@C112-012>
2026-01-20 09:11:08 +0100

    Added Card class and Actions enum

---
+++ CardGame/CardGame/Model/Card.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CardGame.Model
+{
+    public class Card
+    {
+        public enum Actions
+        {
+            Attack,
+            Heal,
+            Shield,
+            Empty
+        }
+
+        private string _name;
+        private Actions _action;
+        private int _value;
+
+        public string Name { get { return _name; }}
+
+        public Actions Action { get { return _action; }}
+
+        public int Value { get { return _value; }}
+
+        public Card(string name, Actions action, int value)
+        {
+            _name = name;
+            _action = action;
+            _value = value;
+        }
+    }
+}
